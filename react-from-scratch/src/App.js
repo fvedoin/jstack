07c60c592ee7808feccb72d455477e4f7dbc20c8
Fernando <fernando.vgarcia@hotmail.com>
2022-12-07 10:07:08 +0000

    Finishing styles.

--- react-from-scratch/src/App.js
+++ react-from-scratch/src/App.js
@@ -1,58 +1,15 @@
-import React, { useState } from "react";
-import Post from "./Post";
-import Header from "./Header";
-import { ThemeProvider } from "./ThemeContext";
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+import Layout from './components/Layout';
 
 function App() {
-
-    const [posts, setPosts] = useState([
-        {
-            id: Math.random(),
-            title: "Title#01",
-            subtitle: "Sub#01",
-            likes: 20,
-            read: false
-        }
-    ]);
-
-    function handleRefresh() {
-        setPosts((prevState) => [
-            ...prevState,
-            {
-                id: Math.random(),
-                title: "Title#01",
-                subtitle: "Sub#01",
-                likes: 20,
-                read: false
-            },
-        ]);
-    }
-
-    function handleRemovePost(postId) {
-        setPosts((prevState) => (
-            prevState.filter((post) => post.id !== postId)
-        ));
-    }
-
-    return (
-        <ThemeProvider>
-            <Header title="JStack's blog" >
-                <h2>
-                    Posts da semana
-                    <button onClick={handleRefresh}>Atualizar</button>
-                </h2>
-            </Header>
-            <hr />
-
-            {posts.map(post => (
-                <Post
-                    key={post.id}
-                    onRemove={handleRemovePost}
-                    post={post}
-                />
-            ))}
-        </ThemeProvider>
-    );
-}
+  return (
+    <>
+      <GlobalStyle />
+      <Layout />
+    </>
+  );
+};
 
 export default App;
--- react-from-scratch/webpack.config.js
+++ react-from-scratch/webpack.config.js
@@ -1,30 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin =  require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, "src", "index.js"),
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "bundle[hash].js",
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "public", "index.html"),
-        }),
-        new CleanWebpackPlugin(),
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle[hash].js',
+  }, 
+  plugins: [
+    new HtmlWebpackPlugin ({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          'sass-loader',
+        ],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: "babel-loader"
-            }
-        ]
-    },
-    devServer: {
-        port: 3000
-    },
+    
+  },
+  devServer: {
+    port: 3000,
+  }
 };
