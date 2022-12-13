5f133b683008ca915bcbc76d04c9516506b6ff64
Fernando <fernando.vgarcia@hotmail.com>
2022-12-13 16:57:25 +0000

    Handling form errors with custom hooks.

--- mycontacts-fe/src/components/ContactForm/index.js
+++ mycontacts-fe/src/components/ContactForm/index.js
@@ -1,4 +1,7 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import useErrors from '../../hooks/useErrors';
+import isEmailValid from '../../utils/isEmailValid';
 import Button from '../Button';
 import FormGroup from '../FormGroup';
 import Input from '../Input';
@@ -6,20 +9,77 @@ import Select from '../Select';
 import { Form, ButtonContainer } from './styles';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const { getErrorMessageByFieldName, removeError, setError } = useErrors();
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'name', message: 'Nome é obrigatório' });
+    } else {
+      removeError('name');
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      setError({ field: 'email', message: 'E-mail inválido' });
+    } else {
+      removeError('email');
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input type="text" placeholder="Nome" />
+    <Form onSubmit={(event) => handleSubmit(event)}>
+      <FormGroup
+        error={getErrorMessageByFieldName('name')}
+      >
+        <Input
+          type="text"
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => handleNameChange(event)}
+          error={getErrorMessageByFieldName('name')}
+        />
+      </FormGroup>
+      <FormGroup
+        error={getErrorMessageByFieldName('email')}
+      >
+        <Input
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => handleEmailChange(event)}
+          error={getErrorMessageByFieldName('email')}
+        />
       </FormGroup>
       <FormGroup>
-        <Input type="text" placeholder="E-mail" />
-      </FormGroup>
-      <FormGroup error="teste">
-        <Input danger type="text" placeholder="Telefone" />
+        <Input
+          type="text"
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Select>
-          <option>a</option>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
+          <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
       <ButtonContainer>
--- mycontacts-fe/src/components/Input.js
+++ mycontacts-fe/src/components/Input.js
@@ -17,7 +17,7 @@ export default styled.input`
         border-color: ${({ theme }) => theme.colors.primary.main};
     }
 
-    ${({ theme, danger }) => danger && css`
+    ${({ theme, error }) => error && css`
         color: ${theme.colors.danger.main};
         border-color: ${theme.colors.danger.main} !important;
     `}
