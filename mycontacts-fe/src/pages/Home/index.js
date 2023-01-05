e058d5dda5e049e9ea232839279472fa27903f56
Fernando <fernando.vgarcia@hotmail.com>
2023-01-05 14:40:08 +0000

    Adding mappers to frontend layer.

--- mycontacts-fe/src/pages/Home/index.js
+++ mycontacts-fe/src/pages/Home/index.js
@@ -209,8 +209,8 @@ export default function Home() {
                 <main>
                   <div className="contact-name">
                     <strong>{contact.name}</strong>
-                    {contact.category_name && (
-                    <small>{contact.category_name}</small>
+                    {contact.category.name && (
+                    <small>{contact.category.name}</small>
                     )}
                   </div>
                   <span>{contact.email}</span>
--- mycontacts-fe/src/services/CategoriesService.js
+++ mycontacts-fe/src/services/CategoriesService.js
@@ -1,3 +1,4 @@
+import CategoryMapper from './mappers/CategoryMapper';
 import HttpClient from './utils/HttpClient';
 
 class CategoriesService {
@@ -5,8 +6,10 @@ class CategoriesService {
     this.httpClient = new HttpClient('http://localhost:3333');
   }
 
-  listCategories(orderBy = 'asc') {
-    return this.httpClient.get(`/categories?orderBy=${orderBy}`);
+  async listCategories(orderBy = 'asc') {
+    const categories = await this.httpClient.get(`/categories?orderBy=${orderBy}`);
+
+    return categories.map(CategoryMapper.toDomain);
   }
 }
 
